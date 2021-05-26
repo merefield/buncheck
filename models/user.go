b743d18766529382cdd3f387732925e11a7eaafb
Robert Barrow <merefield@gmail.com>
2021-05-26 08:06:47 +0000

    fix relationship, but add illegal type difference

--- models/user.go
+++ models/user.go
@@ -7,11 +7,12 @@ import (
 )
 
 type UserGroup struct {
-	ID          int64     `bun:",unique,notnull"`
+	ID          uuid.UUID `bun:",unique,notnull"`
 	CreatedAt   time.Time `bun:"default:now()"`
 	UpdatedAt   time.Time
 	DisplayName string `bun:",unique,notnull"`
 	Description string
+	OwnerID     int64
 }
 
 // User basic definition of a User and its meta
@@ -19,9 +20,9 @@ type User struct {
 	ID             uuid.UUID `bun:"type:uuid,default:uuid_generate_v4()"`
 	CreatedAt      time.Time `bun:"default:now()"`
 	UpdatedAt      time.Time
-	Username       string      `bun:",notnull,unique"`
-	FullName       string      `bun:",notnull"`
-	Email          string      `bun:",unique,notnull"`
-	FollowedGroups []uuid.UUID `bun:",type:uuid[],array"`
-	OwnerOfGroups  []UserGroup `bun:"rel:has-many"`
+	Username       string       `bun:",notnull,unique"`
+	FullName       string       `bun:",notnull"`
+	Email          string       `bun:",unique,notnull"`
+	FollowedGroups []uuid.UUID  `bun:",type:uuid[],array"`
+	OwnerOfGroups  []*UserGroup `bun:"rel:has-many,join:id=owner_id""`
 }
